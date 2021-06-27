331dea3ed243277aa665a75169e62c4c3712bb08
NhutNguyen236 <nhutnguyenf330@gmail.com>
2021-06-27 16:57:43 +0700

    :zap: add Donation form

--- js/main.js
+++ js/main.js
@@ -386,3 +386,32 @@ mailChimp();
 	carousel();
 
 })(jQuery);
+
+// Donation page function
+$(document).ready(function(){
+    //Menu Toggle Script
+    $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+    });
+    
+    // For highlighting activated tabs
+    $("#tab1").click(function () {
+        $(".tabs").removeClass("active1");
+        $(".tabs").addClass("bg-light");
+        $("#tab1").addClass("active1");
+        $("#tab1").removeClass("bg-light");
+    });
+    $("#tab2").click(function () {
+        $(".tabs").removeClass("active1");
+        $(".tabs").addClass("bg-light");
+        $("#tab2").addClass("active1");
+        $("#tab2").removeClass("bg-light");
+    });
+    $("#tab3").click(function () {
+        $(".tabs").removeClass("active1");
+        $(".tabs").addClass("bg-light");
+        $("#tab3").addClass("active1");
+        $("#tab3").removeClass("bg-light");
+    });
+})
